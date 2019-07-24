89bd8c055d6f9cbd737f5d800a0b9eb0c63c39c1
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2019-07-24 16:15:30 +0900

    Trans Date data and Deleat wrong data

--- book/1chapter/ufo.R
+++ book/1chapter/ufo.R
@@ -1,5 +1,15 @@
 library(ggplot2)
-
 ufo<-read.delim("data/ufo_awesome.tsv", sep="\t", stringsAsFactors=FALSE, header=FALSE, na.string="")
+
+names(ufo) <-c("DateOccured", "DateReported","location","ShortDescription", "Duration", "LongDescription")
+# head(ufo[which(nchar(ufo$DateOccured) != 8 | nchar(ufo$DateReported) != 8), 1])
+
+good.rows <-ifelse(nchar(ufo$DateOccured) != 8 | nchar(ufo$DateReported) != 8, FALSE, TRUE)
+# length(which(!good.rows))
+
+ufo <-ufo[good.rows,]
+
+ufo$DateOccured <-as.Date(ufo$DateOccured, format="%Y%m%d")
+ufo$DateOccured <-as.Date(ufo$DateReported, format="%Y%m%d")
 
 head(ufo)
