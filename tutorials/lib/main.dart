e452e1d11d9845bd662e6123c709294b94add153
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2019-08-13 15:13:07 +0900

    Pairing word in random as PascalCase

--- tutorials/lib/main.dart
+++ tutorials/lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text("Hello World! I'm syu-kwsk"),
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
